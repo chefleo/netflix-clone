a8fa1f85ba87c63a9f61120538423d8ce72f5b2f
Leonardo Digiorgio <simon.digiorgio@gmail.com>
2021-10-19 10:35:06 +0200

    Create netflix-clone

---
+++ src/Row.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import './Row.css';
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
+
+  useEffect(() => {
+    //if [], run once when loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //'https://api.themoviedb.org/3 + /trending/all/week?api_key=${API_KEY}&language=en-US'
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+        .then((url) => {
+          // https://www.youtube.com/watch?v=asSDasSc
+          const urlParams = new URLSearchParams(new URL(url).search); // To get url after ?
+          console.log(urlParams.get('v'));
+          setTrailerUrl(urlParams.get('v')); // asSDasSc
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* container -> posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
